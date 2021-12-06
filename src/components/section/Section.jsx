b217a60441fbe38aa9d07a4f0159c73798bfe39a
Fedeaguilera94 <aguilerafederico94@gmail.com>
2021-12-06 00:54:47 -0300

    hover css

--- src/components/section/Section.jsx
+++ src/components/section/Section.jsx
@@ -1,8 +1,19 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./Section.css";
+
 export default function Section({ url, title }) {
   const [movie, setMovie] = useState([]);
+
+  const [scrollX, setScrollX] = useState(-400);
+
+  const handleLeftArrow = () => {
+    let x = scrollX + 150;
+    if (x > 0) {
+      x = 0;
+    }
+    setScrollX(x);
+  };
 
   useEffect(() => {
     async function Getdata() {
@@ -12,19 +23,21 @@ export default function Section({ url, title }) {
     Getdata();
   }, []);
 
-  console.log(title, movie);
   const img_url = "https://image.tmdb.org/t/p/original";
 
   return (
     <div className="section_row">
       <h3 className="title">{title}</h3>
-      <div className="section_img">
+      <div className="row">
         {movie.map((m) => (
-          <img
-            className="movie_img"
-            src={`${img_url}${m.backdrop_path}`}
-            alt={m.name}
-          />
+          <div className="section_img">
+            <img
+              className="movie_img"
+              /* backdrop_path || poster_path */
+              src={`${img_url}${m.poster_path}`}
+              alt={m.name}
+            />
+          </div>
         ))}
       </div>
     </div>
