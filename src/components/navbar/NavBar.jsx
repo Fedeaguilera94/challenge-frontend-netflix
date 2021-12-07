b010a8cf01c89ccaacf933f8eb59a73fdba78fd1
Fedeaguilera94 <aguilerafederico94@gmail.com>
2021-12-06 23:41:11 -0300

    fix navbar mobile

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react/cjs/react.development";
 import netflix from "../../img/logo.png";
 import "./NavBar.css";
 
@@ -7,31 +8,57 @@ export default function NavBar() {
     const header = document.querySelector(".navbar");
     header.classList.toggle("active", this.window.scrollY > 0);
   });
+  const [width, setWidth] = useState(window.innerWidth);
+  const size = () => {
+    setWidth(window.innerWidth);
+  };
+  useEffect(() => {
+    window.addEventListener("resize", size);
+  }, []);
 
   return (
     <div className="navbar_bg">
       <div className="navbar">
         <img className="logo" src={netflix} alt="logo" />
-        <ul className="menu">
-          <a>
-            <li>Inicio</li>
-          </a>
-          <a>
-            <li>Ver de nuevo</li>
-          </a>
-          <a>
-            <li>Series</li>
-          </a>
-          <a>
-            <li>Peliculas</li>
-          </a>
-          <a>
-            <li>Novedades populares</li>
-          </a>
-          <a>
-            <li>Mi lista</li>
-          </a>
-        </ul>
+        {width > "425" ? (
+          <>
+            <ul className="menu">
+              <a>
+                <li>Inicio</li>
+              </a>
+              <a>
+                <li>Ver de nuevo</li>
+              </a>
+              <a>
+                <li>Series</li>
+              </a>
+              <a>
+                <li>Peliculas</li>
+              </a>
+              <a>
+                <li>Novedades populares</li>
+              </a>
+              <a>
+                <li>Mi lista</li>
+              </a>
+            </ul>
+          </>
+        ) : (
+          <ul className="menu">
+            <a>
+              <li> Inicio</li>
+            </a>
+            <a>
+              <li> Peliculas</li>
+            </a>
+            <a>
+              <li> Ver de nuevo</li>
+            </a>
+            <a>
+              <li> Ver mas 🡓</li>
+            </a>
+          </ul>
+        )}
       </div>
     </div>
   );
